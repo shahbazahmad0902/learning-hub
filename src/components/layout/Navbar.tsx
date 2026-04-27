3b204f63dfa18fee163568f96a03b24ff3ec884b
Shahbaz Ahmad <ahmad.shahbaz0902@gmail.com>
2026-04-27 14:36:22 +0530

    feat: implement TypeScript types, generics, and utility types

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar = () => {
                 ) : (
                   <Button variant="contained" onClick={login}>
                     Login   
-                    are 
+                    ar
                   </Button>
                 )}
               </Toolbar>
