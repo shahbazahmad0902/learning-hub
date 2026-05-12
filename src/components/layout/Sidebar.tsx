3640e57762b2795370a09e8a354f81b5b4b1502e
Shahbaz Ahmad <ahmad.shahbaz0902@gmail.com>
2026-05-12 14:39:24 +0530

    feat: implement TypeScript types, generics, and utility types

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -16,6 +16,7 @@ const Sidebar = () => {
       <Typography fontWeight="bold" mb={3}>
         🚀 Learning Hub  dsadsdsadgdfgfdgfdgfdgg
         fsdfsdfsdfsdfsdfsdfsdfsdfsdffsfsfdsfdsfddfs
+        dfdfsdfsdfsdfsdf
       </Typography>
 
       <Button fullWidth onClick={() => navigate("/")}>
