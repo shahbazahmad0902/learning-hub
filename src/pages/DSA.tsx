dc2370f4ead0839a9f558a4ba87874b368c69503
Shahbaz Ahmad <ahmad.shahbaz0902@gmail.com>
2026-06-03 19:39:55 +0530

    feat: implement TypeScript types, generics, and utility types

--- src/pages/DSA.tsx
+++ src/pages/DSA.tsx
@@ -3,7 +3,7 @@ import { dsaTopics } from "../data/dsa";
 const DSA = () => {
   return (
     <div style={{ padding: "20px" }}>
-      <h1>🧠 DSA Practice</h1>sdasdsadasdasasdsadasdsadsad
+      <h1>🧠 DSA Practice</h1>sdasdsadasdasasdsadasdsadsadasdasdsadsadsadsad
 
       {dsaTopics.map((topic) => (
         <div key={topic.id} style={{ marginBottom: "20px" }}>
