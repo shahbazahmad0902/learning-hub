b40b1160a5a89d45b58e95b63b7e937d0abdfaa6
Shahbaz Ahmad <ahmad.shahbaz0902@gmail.com>
2026-06-06 21:45:55 +0530

    feat: implement TypeScript types, generics, and utility types

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -62,7 +62,7 @@ const Home = () => {
       <Container maxWidth="lg" sx={{ py: 8 }}>
         {/* Section Title */}
         <Typography variant="h5" fontWeight="bold" mb={4}>
-          📚 Explore Topicsfsdfdfsdfsddasdsadasdsadasdsdasdasdasddadsaasdasdadasdsadasddasadsadsadasdsa
+          📚 Explore Topicsfsdfdfsdfsddasdsadasdsadasdsdasdasdasddadsaasdasdadasdsadasddasadsadsadasdsaasdsadsadsadas
         </Typography>
 
         {/* Cards */}
