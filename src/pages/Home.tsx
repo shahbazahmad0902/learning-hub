f0db7055c7b33e2e42e304144a6222a579203520
Shahbaz Ahmad <ahmad.shahbaz0902@gmail.com>
2026-06-19 16:54:50 +0530

    feat: implement TypeScript types, generics, and utility types

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -63,7 +63,7 @@ const Home = () => {
         {/* Section Title */}
         <Typography variant="h5" fontWeight="bold" mb={4}>
           📚 Explore Topicssadsadsaasdasdasd
-        </Typography>
+        </Typography>asdas
 
         {/* Cards */}
         <Box
