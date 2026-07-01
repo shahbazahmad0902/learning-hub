4fc465ee9a1eeab6dd0c1f8d4e52b6c005d6f71a
Shahbaz Ahmad <ahmad.shahbaz0902@gmail.com>
2026-07-01 18:25:20 +0530

    feat: implement TypeScript types, generics, and utility types

--- src/pages/Topic.tsx
+++ src/pages/Topic.tsx
@@ -184,7 +184,7 @@ const Topic = () => {
         {/* ❌ No Result */}
         {filteredTopics.length === 0 && (
           <Typography textAlign="center" mt={4} color="text.secondary">
-            No topics found...SasASsASadsaasdiopollllasdsadsaddsasdsadasqewqqweweqweqwqewqeqweasasdasd
+            No topics found...SasASsASadsaasdiopollllasdsadsaddsasdsadasqewqqweweqweqwqewqeqweasasdasddqwqwd
           </Typography>
         )}
       </Container>
